78d4b0ef10d85cb0dfb837f2485bb785e21d9545
rancongjie <95633676@qq.com>
2016-09-09 02:26:10 +0800

    datebase

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
-python /code/manage.py db init
-python /code/manage.py db migrate
-python /code/manage.py db upgrade
+python /code/app/main/models.py
+#python /code/manage.py db init
+#python /code/manage.py db migrate
+#python /code/manage.py db upgrade
 /usr/local/bin/gunicorn -w 1 -b :8000 wsgi:application
