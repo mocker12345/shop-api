c7d916b78366f6cef6a86bcf03e125227df7beed
rancongjie <95633676@qq.com>
2016-09-08 21:52:02 +0800

    dao

--- manage.py
+++ manage.py
@@ -18,8 +18,8 @@ def make_shell_context():
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-manager.add_command('runserver', app.run(host='127.0.0.1', port=5000))
+manager.add_command('runserver', app.run(host='127.0.0.1', port=8000))
 
 
-if __name__ == '__main__':
-    manager.run()
+# if __name__ == '__main__':
+
