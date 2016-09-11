be3d008d342ba75f4e0a30e37a543b62cdf5e0dd
rancongjie <95633676@qq.com>
2016-09-11 16:19:32 +0800

    update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
-python /code/app/models.py
+# python /code/app/models.py
 #python /code/manage.py db init
 #python /code/manage.py db migrate
-#python /code/manage.py db upgrade
-/usr/local/bin/gunicorn -w 1 -b :8000 wsgi:application
+python /code/manage.py
+# /usr/local/bin/gunicorn -w 1 -b :8000 wsgi:application
