18873593c2d2dbaffaf6a8c79e9f56a4dcfb743f
rancongjie <95633676@qq.com>
2016-09-24 20:59:56 +0800

    author

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
 FROM daocloud.io/python:2.7
+
+MAINTAINER Rancongjie "rancongjie@126.com"
+
 ADD requirements.txt /requirements.txt
+
 RUN pip install -r /requirements.txt
+
 RUN mkdir /code
+
 WORKDIR /code
 COPY . /code
 COPY docker-entrypoint.sh docker-entrypoint.sh
 RUN chmod +x docker-entrypoint.sh
 EXPOSE 8000
+
 CMD /code/docker-entrypoint.sh
--- test/category/test_views.py
+++ test/category/test_views.py
@@ -6,19 +6,6 @@ from app import db
 import unittest
 
 class test_category(TestCase):
-    # def test_add_category(self):
-    #     category = Category(name='rancongjie')
-    #     db.session.add(category)
-    #     db.session.commit()
-    #     assert category in db.session
-
-    # def test_update_category(self):
-    #     category = db.session.query(Category).get(1)
-    #     category.name = '222'
-    #     db.session.add(category)
-    #     db.session.commit()
-    #     assert category in db.session
-
-    def test_category_view(self):
+    def test_category_get(self):
         response = urllib2.urlopen(self.get_server_url() + '/category')
         self.assertEqual(response.code, 200)
